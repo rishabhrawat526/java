bd79bea499000b77efcafca83f50c7addec48cd9
Rishabh <rishabhrawat526@gmail.com>
2022-03-12 09:42:24 +0530

    this is my first java program

--- Learning Java/src/UNIIT_01/P1_Task01_FirstJavaProgram.java
+++ Learning Java/src/UNIIT_01/P1_Task01_FirstJavaProgram.java
@@ -3,8 +3,8 @@ package UNIIT_01;
 public class P1_Task01_FirstJavaProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world!");
+		
+		System.out.println("Hello World! This is My first Java Program");
 
 	}
 

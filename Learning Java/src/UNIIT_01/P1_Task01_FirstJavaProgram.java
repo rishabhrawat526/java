59403689adeea9ee345d2d7bc2dd0829d32ecb28
Rishabh <rishabhrawat526@gmail.com>
2022-03-12 09:40:26 +0530

    LearningJava

---
+++ Learning Java/src/UNIIT_01/P1_Task01_FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package UNIIT_01;
+
+public class P1_Task01_FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+
+	}
+
+}

fbcaefc4b3eff3a71249e6e3e5b9d341fe4cd123
Rishabh <rishabhrawat526@gmail.com>
2022-03-26 17:28:49 +0530

    save

---
+++ Learning Java/src/UNIIT_01/p2_task01_secondjavaprogram.java
@@ -0,0 +1,30 @@
+package UNIIT_01;
+
+public class p2_task01_secondjavaprogram {
+	
+//int double=10;
+//error because double is a reserved keyword and cannot be used as a variable name
+	public static void main(String[] args)
+	{
+		
+		int a=10;
+		System.out.println(a);
+		hello();
+	}
+//	static void int()
+//	{
+//		System.out.println("hello !world ");
+//	}
+	
+//	above function shows error because int is a reserved word
+//	and cannot be used as function name
+	static void hello()
+	{
+		System.out.println("hello !world ");
+	}
+	
+	static class abc
+	{
+		Sysout
+	}
+}
